fbc40c5059586df01ed4a44cc7d7e897ce1a4597
Brexton <brextonpham@stanford.edu>
2015-06-16 21:11:57 -0400

    Initial Commit

---
+++ FratChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FratChat
+//
+//  Created by Brexton Pham on 6/16/15.
+//  Copyright (c) 2015 Brexton Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FratChat/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  FratChat
+//
+//  Created by Brexton Pham on 6/16/15.
+//  Copyright (c) 2015 Brexton Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
